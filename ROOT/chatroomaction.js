d1e94f76c3f01cd86913eccb6f49879689af0bb3
CHIRUG <chiranjeevi.gatti@gmail.com>
2015-08-11 17:22:28 +0530

    Done with modifications!

--- ROOT/chatroomaction.js
+++ ROOT/chatroomaction.js
@@ -3,28 +3,14 @@ $(document).ready(function(){
 
 	 $.ajax({url:"/history",
 						success: function(data){
-							// alert(data);
+						//alert(data);
 						$('#msg').val("");
-						var data1 = data;
-						data1 = data1.replace(/\[/gi,"");
-						data1 = data1.replace(/\]/gi,"");
-						end=0;
-						msgarr=data1.split(",");
-						for(start = 0; start < msgarr.length; start = start+4)
-						{
-							end+=4;
-							msg = msgarr.slice(start,end);
-							uname = msg[0];
-							sentmsg = msg[1];
-							time = msg[2];
-							date = msg[3];
-							$("#all_msg").append(uname+":  "+sentmsg+" "+time+","+date+"\n");
-						}
+						ShowMessages(data);
 					}
 					
 				});
 
-	$("#all_msg").animate({scrollTop:1000000000});
+	$("#all_msg").animate({scrollTop:19999999999990});
 	$("#btn_send").click(function(){
 		var sentmsg = $("#msg").val(); 
 		$('#msg').val("");
@@ -32,26 +18,16 @@ $(document).ready(function(){
 					success: function(data)
 					{
 						// alert(data);
-						var data1 = data;
-						data1 = data1.replace(/\[/gi,"");
-						data1 = data1.replace(/\]/gi,"");
-						end=0;
-						msgarr=data1.split(",");
-						// alert(msgarr[1]);
-						//alert(msgarr.length);
-						for(start = 0; start < msgarr.length; start = start+4)
+						if(data == "1")
 						{
-							end+=4;
-							msg = msgarr.slice(start,end);
-							uname = msg[0];
-							sentmsg = msg[1];
-							time = msg[2];
-							date = msg[3];
-							// alert(sentmsg);
-							$("#all_msg").append(uname+": "+sentmsg+" "+time+","+date+"\n");
+							window.location = "login.html";
 						}
-						window.location.reload();
-						$("#all_msg").animate({scrollTop:1000000000});
+						else
+						{
+							ShowMessages(data);
+							window.location.reload();
+							$("#all_msg").animate({scrollTop:1000000000});
+						}
 					}
 
 		});
@@ -66,3 +42,24 @@ $(document).ready(function(){
 			});		
 		});
 	});
+	function ShowMessages(data)
+	{
+		var data1 = data;
+		data1 = data1.replace(/\[/gi,"");
+		data1 = data1.replace(/\]/gi,"");
+		end=0;
+		msgarr=data1.split(",");
+		// alert(msgarr[1]);
+		//alert(msgarr.length);
+		for(start = 0; start < msgarr.length; start = start+4)
+		{
+			end+=4;
+			msg = msgarr.slice(start,end);
+			uname = msg[0];
+			sentmsg = msg[1];
+			time = msg[2];
+			date = msg[3];
+			// alert(sentmsg);
+			$("#all_msg").append(uname+": "+sentmsg+" "+time+","+date+"\n");
+		}
+	}
